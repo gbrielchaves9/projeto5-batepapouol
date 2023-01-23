4dc82b6f94291ebf257387d335c598cf6a7ffeb3
gbrielchaves9 <gbrielchaves9@gmai.com>
2023-01-23 16:40:07 -0300

    layout ok 2

--- javas.js
+++ javas.js
@@ -119,3 +119,157 @@ function pedirnome(){
 pedirnome()
 
     //const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants');
+
+function carrega(verificar){
+    const msgantigas = verificar.data 
+    const minhalista = document.querySelector('.sala')
+    minhalista.innerHTML='';
+    for(let i = 0 ; i < msgantigas.length ;i++) 
+    {  
+        if(objetos.data[i].type === "status"){
+            minhalista.innerHTML
+        
+        `
+        <li class="msg cinza">
+        <div class="hora">(${objetos.time})</div>
+        <div class="quem">${objetos.from} </div>
+        <div class="texto">${objetos.text}</div>
+    </li>
+        `
+    }
+    else if (objetos.from === 'Todos' ){
+            
+        padrao = `
+        <li class="msg rosa">
+        <div class="hora">(${objetos.time})</div>
+        <div class="quem">${objetos.from} </div>
+        <div class="tipo">para</div>
+        <div class="para">${objetos.to}</div>
+        <div class="texto">${objetos.text}</div>
+        </li>
+        `;
+    }
+    else if (objetos.to === 'Todos'){
+
+        padrao = `
+        <li class="msg branca">
+        <div class="hora">(${objetos.time})</div>
+        <div class="quem">${objetos.from} </div>
+        <div class="tipo">para</div>
+        <div class="para">${objetos.to}</div>
+        <div class="texto">${objetos.text}</div>
+        </li>
+        `;
+    }  
+    minhalista.innerHTML += padrao
+}
+}
+
+
+
+
+//
+
+function carrega(verificar){
+    console.log(verificar)
+    const msgantigas = verificar.data 
+    console.log(msgantigas)
+
+    const minhalista = document.querySelector('.sala')
+    minhalista.innerHTML='';
+
+    for(let i = 0 ; i < msgantigas.length ;i++) 
+    {
+        let objetos = msgantigas[i];
+        let padrao ;
+
+        if ( mensagem.to !== 'Todos'){
+        
+            padrao =
+            `
+            <li class="conversa-publica"  data-test="message">
+            <span class="horario">( ${objetos.time} )</span>
+                <strong>${objetos.from}</strong>
+                <span> para </span>
+                <strong>${objetos.to}: </strong>
+            <span>${objetos.text}</span>
+        </li>        
+                     
+            `;
+
+        }else if ( mensagem.from === null ){
+            padrao =
+            `
+            <li class="entrada-saida"  data-test="message">
+                    <span class="horario">( ${objetos.time} )</span>
+                    <strong>( ${objetos.from} )</strong>          
+                    <span>${objetos.text}</span>            
+                </li>
+            `
+        }else if ( mensagem.to === 'Todos'){
+            padrao = `
+            <li class="conversa-privada"  data-test="message">
+                    <span class="horario">( ${objetos.time} )</span>
+                        <strong>${objetos}</strong>
+                            <span> reservadamente para </span>
+                        <strong>${objetos.to}: </strong>
+                    <span>${objetos.text}</span>
+                </li>
+            `;
+        }
+
+
+        minhalista.innerHTML += padrao
+    }
+}
+
+
+let usuario;
+function buscarmsg(){
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promise.then(carrega)
+}
+
+function msgChegaram(resposta){
+    console.log('chegaram!!!!!!');
+    console.log(resposta);
+}
+
+function deuErroPegarmsg(erro){
+    console.log('Deu erro ao pergar no servidor');
+    console.log(erro);
+}
+
+
+function coloqueinome(){
+
+   const promise =  axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {name:usuario})
+
+   promise.then(msgChegaram);
+    promise.catch(deuErroPegarmsg);
+}
+function msgChegaram2(resposta2){
+    console.log('status ok');
+    console.log(resposta2);
+}
+
+function deuErroPegarmsg2(erro2){
+    console.log('Deu erro ao pergar no status');
+    console.log(erro2);
+}
+
+function atualizar(){
+    const promise =  axios.post('https://mock-api.driven.com.br/api/v6/uol/status', {name:usuario})
+    
+    promise.then(msgChegaram2);
+    promise.catch(deuErroPegarmsg2);
+}
+
+function renderizar(){
+    usuario = prompt('qual o seu nome?');
+buscarmsg();
+coloqueinome();
+
+setInterval(atualizar,3000);
+}
+renderizar();
--- js.js
+++ js.js
@@ -1,32 +1,105 @@
-function carrega(verificar){
-    const msgantigas = verificar.data 
-    const minhalista = document.querySelector('.sala')
-    minhalista.innerHTML='';
-    for(let i = 0 ; i < msgantigas.length ;i++) 
-    {
-        let objetos = msgantigas[i];
-        let padrao = `
-        <li class="msg cinza">
-        <div class="hora">(${objetos.time})</div>
-        <div class="quem">${objetos.from} </div>
-        <div class="tipo">para</div>
-        <div class="para">${objetos.to}</div>
-        <div class="texto">${objetos.text}</div>
-    </li>
-        `
-        minhalista.innerHTML += padrao
-    }
-    const elementoQueQueroQueApareca = document.querySelector('.msg');
-elementoQueQueroQueApareca.scrollIntoView();
+let msg = [];
+function pergarDados(resposta) { 
+    console.log(resposta)
+    const promessa = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
+    promessa.then(dadosChegaram) 
 }
+pergarDados()  
+function dadosChegaram(resposta) { 
+    msg = resposta.data;
+    renderiza();
+}
+renderiza();
+function renderiza(){   
+    const ul = document.querySelector('.sala');
+    ul.innerHTML = "";
+    for( let i=0; i < msg.length; i++){
+        const from = msg[i].from;
+        const to = msg[i].to;        
+        const text = msg[i].text;
+        const type = msg[i].type;        
+        const time = msg[i].time;
+        if( type === 'status'){
+            ul.innerHTML += `
+            <li class="msg cinza">
+            <span class="horario">${time}</span>
+            <p>
+                <strong class="quem">${from}</strong>
+                ${text}
+            </p>
+        </li>
+            ` 
+        } else if( type === 'message'){
+            ul.innerHTML += `
+             <li class="msg branca">
+            <span class="horario">${time}</span>
+            <p>
+                <strong class="quem">${from}</strong>
+                 para <strong class="para">${to}</strong>:
+                  ${text}
+            </p>
+        </li>
+            `
+        } else if( type === 'private_message'){
+            ul.innerHTML += `
+            <li class="msg rosa ">
+            <span class="horario">${time}</span>
+            <p>
+                <strong class="quem">${from}</strong>
+                 reservadamente para <strong class="para">${to}</strong>:
+                  ${text}
+            </p>
+        </li>
+            `
+        };
+    };
+};
+let usuario;
 function buscarmsg(){
     const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
-    promise.then(carrega)
+    promise.then(renderiza)
 }
+
+function msgChegaram(resposta){
+    console.log('chegaram!!!!!!');
+    console.log(resposta);
+}
+
+function deuErroPegarmsg(erro){
+    console.log('Deu erro ao pergar no servidor');
+    console.log(erro);
+}
+
+
+function coloqueinome(){
+
+   const promise =  axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {name:usuario})
+
+   promise.then(msgChegaram);
+    promise.catch(deuErroPegarmsg);
+}
+function msgChegaram2(resposta2){
+    console.log('status ok');
+    console.log(resposta2);
+}
+
+function deuErroPegarmsg2(erro2){
+    console.log('Deu erro ao pergar no status');
+    console.log(erro2);
+}
+
+function atualizar(){
+    const promise =  axios.post('https://mock-api.driven.com.br/api/v6/uol/status', {name:usuario})
+    
+    promise.then(msgChegaram2);
+    promise.catch(deuErroPegarmsg2);
+}
+
 function renderizar(){
+    usuario = prompt('qual o seu nome?');
 buscarmsg();
+coloqueinome();
+
+setInterval(atualizar,3000);
 }
 renderizar();
-
-
-
